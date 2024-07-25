ea27e15d902f49fb7ac80434d528b0588efa5ac7
Luca Sorace "Stranck <strdjn@gmail.com>
2024-07-25 10:18:55 +0200

    Updated DOCS_TEST_CARDS_URL link

--- pretix_xpay/constants.py
+++ pretix_xpay/constants.py
@@ -5,7 +5,7 @@ ENDPOINT_ORDERS_CREATE = f"ecomm/DispatcherServlet"
 ENDPOINT_ORDERS_CONFIRM = f"api/bo/contabilizza"
 ENDPOINT_ORDERS_CANCEL = f"api/bo/storna"
 
-DOCS_TEST_CARDS_URL = "https://developer.nexi.it/en/area-test/carte-di-pagamento"
+DOCS_TEST_CARDS_URL = "https://ecommerce.nexi.it/area-test"
 
 HASH_TAG = "plugins:pretix_xpay"
 
